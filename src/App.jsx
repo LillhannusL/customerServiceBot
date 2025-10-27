e7eb8d923440eeaa7f5548e83a45c5f625e5eda6
Lisa-Ni <niklasson.lisa@gmail.com>
2025-10-27 11:24:53 +0100

    pseudokod

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
 import './App.css';
 
+//hämta modell
+//PromptTemplates, answerPrompt
+
 function App() {
+	//InputRef och Message,Loading state
+
+	//combineDocuments funktion
+
+	//sendAnswer Fuktion
+	//hämta text från input
+	//lägga användarens fråga i chathistoriken
+
+	//skapa kedja av runnablesequence
+	//Gör om frågan till standalonequestion
+	//omvandla svaret till text
+	//hämta relaterade dokument, kombinera till en sammanhängade text
+	//skapa slutsteg
+	//skicka frågan + context till modellen
+	//få tillbaka svar
+	//Kör Kedjan med användarens fråga som input
+	//Lägg till modellens svar i chatthistoriken
+	//sätt loading till false
+
+	//message component
+
 	return (
 		<main className="chat">
 			<section className="chat__messages">messages</section>
--- src/Utils/setUpReciver.js
+++ src/Utils/setUpReciver.js
@@ -1,1 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
+
+//hämta miljövariabler
+//skapa embedding, ollama modell
+//skapa supabaseClient
+//skapa vectorstore skicka med embedding och object med innehåll client, tabelname och queryname
+//hämta document
+//exportera variabel med hämtade dokument
