60ba6b11972f475cce66e39c39000c39e380e769
Lisa-Ni <niklasson.lisa@gmail.com>
2025-10-29 15:02:36 +0100

    loadingstate

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,19 @@ import {
 	getChatHistory,
 } from './Utils/memory.js';
 import Message from './Components/Message.jsx';
+import Loading from './Components/Loading.jsx';
 
 function App() {
 	//InputRef och Message,Loading state
 	const inputRef = useRef();
 	const [messages, setMessages] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		inputRef.current?.scrollIntoView({
+			behavior: messages.lenght > 1 ? 'smooth' : 'auto',
+		});
+	}, [messages]);
 
 	//sendAnswer Fuktion
 	async function sendAnswer(e) {
@@ -22,6 +30,7 @@ function App() {
 		const question = inputRef.current.value;
 		console.log('fråga: ', question);
 		if (!question) return;
+		setIsLoading(true);
 
 		//lägga användarens fråga i chathistoriken
 		setMessages((prevState) => {
@@ -37,10 +46,11 @@ function App() {
 			return [...prevState, { role: 'KundtjänstBot', content: answer }];
 		});
 		await addAiMessage(answer);
-
 		const updatedMessages = await getChatHistory();
 		setMessages(updatedMessages);
 		console.log('memory: ', memory);
+
+		setIsLoading(false);
 	}
 
 	//message component
@@ -52,7 +62,10 @@ function App() {
 
 	return (
 		<main className="chat">
-			<section className="chat__messages">{messageComponent}</section>
+			<section className="chat__messages">
+				{messageComponent}
+				{isLoading && <Loading />}
+			</section>
 			<form className="chat__form">
 				<input
 					type="text"
